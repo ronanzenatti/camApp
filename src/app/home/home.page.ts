4a511ad96885ddfba024668117df5b83ae38dde3
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2023-05-23 22:12:04 -0300

    fotos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FotoService } from '../services/foto.service';
+import { Foto } from '../models/Foto.model';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,31 @@ import { FotoService } from '../services/foto.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(public fotoService: FotoService) {}
+  constructor(public fotoService: FotoService, public actionSheetController: ActionSheetController) { }
 
-  tirarFoto() {
-    this.fotoService.tirarFoto();
+  async ngOnInit() {
+    await this.fotoService.carregarFotosSalvas();
+  }
+
+  public async showActionSheet(foto: Foto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Fotos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.fotoService.deletePicture(foto, position);
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 }
